334c670f3cb0f4a46f12b3a4dbacf5e71208a446
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-08-31 14:46:41 +0430

    signals for user is created

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models import signals
 
 class User(AbstractUser):
 	username = models.CharField(max_length=255)
