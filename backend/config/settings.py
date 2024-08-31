f3a3d7449d8c92a9c89f11f20fb98318d393af5c
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-08-31 12:36:17 +0430

    media and static file setting is done

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,6 +108,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR  'static'
+STATICFILES_DIRS = ('core/static',)
+
+
+MEDIA_URL = 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
